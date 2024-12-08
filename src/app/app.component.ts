bd708a5e747dfbdf9d0ab8dd010623b976476b9c
jhangmez <jhangomez25@gmail.com>
2024-12-07 21:29:19 -0500

    Se mejoro estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -29,9 +34,12 @@ export class AppComponent {
   @ViewChild('sidenav') sidenav!: MatSidenav;
   title = 'Prueba Técnica';
 
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, private router: Router) {}
 
   logout(): void {
     this.authService.logout();
+  }
+  navigateToRoot(): void {
+    this.router.navigate(['/']);
   }
 }
